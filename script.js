ef6e75310e97174acc8fab668b4cbb4c537eb5c7
Emil Rapp <emilemail4@gmail.com>
2021-02-06 13:26:47 +0100

    Lighthouse optimizations
    Added some meta-tags and preloading images. Changed a png to a webp. Added lang="sv" to html element

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function changeImage() {
   var Image_Id = document.getElementById("theImage");
-  if (Image_Id.src.match("./images/first-frame.png")) {
+  if (Image_Id.src.match("./images/first-frame.webp")) {
     Image_Id.src = "./videos/film.gif";
   } else {
-    Image_Id.src = "./images/first-frame.png";
+    Image_Id.src = "./images/first-frame.webp";
   }
 }
